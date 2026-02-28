f1e49f07ac3140654ecc3994d27049aabd2636ef
soumabali <52565929+soumabali@users.noreply.github.com>
2026-03-01 01:50:22 +0800

    chore: sync initial project (resources/views/swagger.blade.php)

---
+++ resources/views/swagger.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>API Docs - {{ strtoupper($version) }}</title>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script>
+    window.ui = SwaggerUIBundle({
+      url: "{{ $specUrl }}",
+      dom_id: '#swagger-ui'
+    });
+  </script>
+</body>
+</html>
