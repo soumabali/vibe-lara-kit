e8d88c797c114a02d3cf592b358817863e3692f6
soumabali <52565929+soumabali@users.noreply.github.com>
2026-03-01 01:48:46 +0800

    chore: sync initial project (database/factories/UserFactory.php)

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Faker\Factory as FakerFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    public function definition(): array
+    {
+        $faker = FakerFactory::create();
+
+        return [
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password123'),
+            'remember_token' => Str::random(10),
+            'is_admin' => false,
+        ];
+    }
+}
